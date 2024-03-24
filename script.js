b5731eab77df64ef3661da858d2e233ee26e92d1
Douglas de Souza Almeida <douglas.souza.almeida@outlook.com.br>
2024-03-24 14:24:11 -0300

    ajuste no campo de busca

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById("result-artist");
+
+const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
-function requestApi(searchTerm) {
-    const url = `http://localhost:2000/artists?name_like=${searchTerm}`
+function requestApi() {
+    const searchInput = document.getElementById('search-input');
+    const searchTerm = searchInput.value.toLowerCase();
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
-        .then((response) => response.json())
-        .then((result) => displayResults(result))
-}
+        .then((response) => {
+            console.log('Resposta da requisição:', response);
+            return response.json();
+        })
+        .then((result) => {
+            console.log('Resultado da busca:', result);
+            displayResults(result);
+        })
+        .catch(error => {
+            console.error('Erro ao realizar a requisição', error);
+        });
+    }
+
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+    resultPlaylist.classList.add('hidden')
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
     result.forEach(element => {
-        artistName.innerText = element.name;
+        artistName.innerText = element.name_exibition;
         artistImage.src = element.urlImg;
     });
 
@@ -23,12 +35,13 @@ function displayResults(result) {
 }
 
 document.addEventListener('input', function () {
+    const searchInput = document.getElementById('search-input');
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
+    if (searchTerm === "") {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
         return
+    } else {
+        requestApi(searchTerm);
     }
-    
-    requestApi(searchTerm);
 })
